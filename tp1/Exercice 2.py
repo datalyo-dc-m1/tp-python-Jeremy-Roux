36545499e069acf2f0106d77b580d978d00f0e88
Jeremy-Roux <jeremy.roux.97@gmail.com>
2020-04-03 16:13:15 +0200

    Corrigé

---
+++ tp1/Exercice 2.py
@@ -0,0 +1,14 @@
+# Exercice 2 : Afficher les nombres pairs entre 0 et 20
+
+print("En utilisant une boucle For : ")
+
+for i in range(0, 21, 2):
+    print(i)
+
+
+print("\n En utilisant une boucle TantQue : ")
+
+i = 0
+while i <= 20:
+    print(i)
+    i = i + 2
