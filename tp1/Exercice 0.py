e7c53978ebbd46cbf09c8afd92e59c39da33e7bc
Jeremy-Roux <jeremy.roux.97@gmail.com>
2020-04-03 15:17:32 +0200

    Exercice 0

---
+++ tp1/Exercice 0.py
@@ -0,0 +1,13 @@
+a = 5.3
+b = 9.70
+c = a + b
+c += a
+print(c)
+# c a une valeur de 20.3
+d = c
+d -= 1
+print(d)
+# d a une valeur de 19.3
+c_int = int(c)
+print(c_int)
+# c_int a une valeur de 20
